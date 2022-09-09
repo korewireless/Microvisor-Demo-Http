3422413d9b51d63e85b0abcb0bb32f4211282359
Tony Smith <tsmith@twilio.com>
2022-09-09 14:30:11 +0100

    Bug fixes; post-lint updates

--- deploy.sh
+++ deploy.sh
@@ -29,9 +29,9 @@ mvplg_minor_min="3"
 mvplg_patch_min="3"
 
 # NOTE
-# This script the build directory is called 'build' and exists within
-# the project directory. If it is used to build the app, this will be
-# the case. You can pass in alternative target for the build product
+# This script assumes the build directory is called 'build' and exists
+# within the project directory. If it is used to build the app, this will
+# be the case. You can pass in alternative target for the build product
 # eg. './deploy.sh build_alt/app/my_app.zip'
 
 # FROM 1.6.0 -- Trap ctrl-c
@@ -77,7 +77,7 @@ set_keys() {
     fi
 
     # Generate the keys using the Twilio CLI Microvisor plugin
-    if twilio microvisor:debug:generate_keypair --debug-auth-privkey="${private_key_path}" --debug-auth-pubkey="${public_key_path}" ; then
+    if twilio microvisor:debug:generate_keypair --debug-auth-privkey="${private_key_path}" --debug-auth-pubkey="${public_key_path}" --force ; then
         echo "Private key written to ${private_key_path}"
         echo " Public key written to ${public_key_path}"
     else
@@ -96,9 +96,9 @@ check_prereqs() {
     [[ ${bv} -lt 4 ]] && show_error_and_exit "This script requires Bash 4+"
 
     #2: required utilities
-    prqs=(jq cmake curl twilio)
+    prqs=(jq cmake twilio)
     for prq in "${prqs[@]}"; do
-        check=$(which "${prq}") || show_error_and_exit "Required utility ${prq} not installed"
+        which "${prq}" > /dev/null 2>&1 || show_error_and_exit "Required utility ${prq} not installed"
     done
 
     #3: credentials set
@@ -119,7 +119,7 @@ build_app() {
         cmake -S . -B build
     fi
     
-    if cmake --build build --clean-first > /dev/null ; then
+    if cmake --build build --clean-first > /dev/null 2>&1 ; then
         echo "App built"
     else
         show_error_and_exit "Could not build the app"
@@ -201,17 +201,17 @@ done
 if [[ ${do_gen_keys} -eq 1 ]]; then
     if [[ "${public_key_path}" = "NONE" ]]; then
         # Public key path not specified, so use the default
-        public_key_path="build/${app_dir}/debug_auth_pub_key.pem"
+        public_key_path="$(pwd)/build/${app_dir}/debug_auth_pub_key.pem"
     fi
     
     if [[ "${private_key_path}" = "NONE" ]]; then
         # Private key path not specified, so use the default
-        private_key_path="build/${app_dir}/debug_auth_prv_key.pem"
+        private_key_path="$(pwd)/build/${app_dir}/debug_auth_prv_key.pem"
     fi
     
     # Create the build directory so we can save keys there
     if [[ ! -e build ]]; then
-        mkdir -p "build/${app_dir}" || show_error_and_exit "Could not create the build directory"
+        mkdir -p "$(pwd)/build/${app_dir}" || show_error_and_exit "Could not create the build directory"
     fi
 
     # Generate the keys
@@ -255,7 +255,7 @@ if [[ ${do_deploy} -eq 1 ]]; then
     else
         # Success... try to assign the app
         echo "Assigning app ${app_sid} to device ${MV_DEVICE_SID}..."
-        if ! assign_action=$(twilio api:microvisor:v1:devices:update --sid=${MV_DEVICE_SID} --target-app=${app_sid}); then
+        if ! twilio api:microvisor:v1:devices:update --sid="${MV_DEVICE_SID}" --target-app="${app_sid}" > /dev/null 2>&1 ; then
             exit 1
         fi
     fi
@@ -265,11 +265,9 @@ fi
 rm -f null.d
 
 # Dump remote debugging command if we can
-echo -e "\nUse the following command to initiate remote debugging:"
-if [[ "${public_key_path}" != "NONE" ]]; then
+if [[ "${public_key_path}" != "NONE" && "${private_key_path}" != "NONE" ]]; then
+    echo -e "\nUse the following command to initiate remote debugging:"
     echo "twilio microvisor:debug ${MV_DEVICE_SID} '${private_key_path}'"
-else
-    echo "twilio microvisor:debug ${MV_DEVICE_SID} '$(pwd)/${private_key_path}'"
 fi
 
 # Start logging if requested to do so
