2d3b8d2435c53971604f1a2f96b27618c396bad4
Tony Smith <tsmith@twilio.com>
2022-09-09 13:11:15 +0100

    Add UART logging code

---
+++ app/uart_logging.h
@@ -0,0 +1,26 @@
+/**
+ *
+ * Microvisor HTTP Communications Demo
+ * Version 2.0.1
+ * Copyright © 2022, Twilio
+ * Licence: Apache 2.0
+ *
+ */
+#ifndef UART_LOGGING_H
+#define UART_LOGGING_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void    UART_init();
+void    UART_output(uint8_t* buffer, uint16_t length);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
